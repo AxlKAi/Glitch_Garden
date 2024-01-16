36c5e91b1b9b924ae9f3712dae329cea482944d1
AxlKAi <36095751+AxlKAi@users.noreply.github.com>
2024-01-16 12:36:24 +0300

    Цвета кнопок скорости изменяются

--- Assets/Script/GameSpeed.cs
+++ Assets/Script/GameSpeed.cs
@@ -1,27 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameSpeed : MonoBehaviour
 {
+    const int MaxGameSpeed = 8;
+
     [SerializeField]
     private NormalSpeedButtonUI _normalSpeedButtonUI;
     [SerializeField]
     private DoubleSpeedButtonUI _doubleSpeedButtonUI;
 
-    private int maxGameSpeed = 16;
+    private Color _activeButton = new Color(.6f, .6f, .6f);
+    private Color _regularButton = new Color(.2f,.2f,.2f);
+    private Color _activeSpeedIndicator = new Color(.75f, .59f, .27f);
+    private float _regularSpeedIndicatorColorMultiplicator = 2.3f;
+
+    private TextMeshProUGUI _speedIndicator;
+    private string _speedSuffix = "x";
 
     public void SetNormalGameSpeed()
     {
         Time.timeScale = 1;
-        _normalSpeedButtonUI.gameObject.GetComponent<Image>().color = Color.red;
+        SetButtonsStateToDefault();
+        _normalSpeedButtonUI.gameObject.GetComponent<Image>().color = _activeButton;
     }
 
     public void SetDoubleGameSpeed()
     {
-        if ((int)Time.timeScale < maxGameSpeed)
+        SetButtonsStateToDefault();
+        _doubleSpeedButtonUI.gameObject.GetComponent<Image>().color = _activeButton;
+
+        if ((int)Time.timeScale < MaxGameSpeed)
             Time.timeScale = Time.timeScale * 2;
+
+        _speedIndicator.text = (int)Time.timeScale + _speedSuffix;
+        _speedIndicator.color = _activeSpeedIndicator;
     }
 
     public void SetZeroGameSpeed()
@@ -33,5 +47,16 @@ public class GameSpeed : MonoBehaviour
     {
         _normalSpeedButtonUI = FindObjectOfType<NormalSpeedButtonUI>();
         _doubleSpeedButtonUI = FindObjectOfType<DoubleSpeedButtonUI>();
+        _speedIndicator = _doubleSpeedButtonUI.gameObject.GetComponentInChildren<TextMeshProUGUI>();
+        SetButtonsStateToDefault();
+        SetNormalGameSpeed();
+    }
+
+    private void SetButtonsStateToDefault()
+    {
+        _normalSpeedButtonUI.gameObject.GetComponent<Image>().color = _regularButton;
+        _doubleSpeedButtonUI.gameObject.GetComponent<Image>().color = _regularButton;
+        _speedIndicator.color = _activeSpeedIndicator/_regularSpeedIndicatorColorMultiplicator;
+        _speedIndicator.text = "1" + _speedSuffix;
     }
 }
