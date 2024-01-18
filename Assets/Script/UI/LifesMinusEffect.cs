a2cb602f1c6d8d63672b7334d29d7e1f3b69cd90
AxlKAi <36095751+AxlKAi@users.noreply.github.com>
2024-01-18 10:01:35 +0300

    эффект улетающей цифры стал лететь по правильной параболе
    эффект улетающей цифры стал лететь по правильной параболе

--- Assets/Script/UI/LifesMinusEffect.cs
+++ Assets/Script/UI/LifesMinusEffect.cs
@@ -1,38 +1,27 @@
 ﻿using UnityEngine;
 using DG.Tweening;
 
-
 public class LifesMinusEffect : MonoBehaviour
 {
     [SerializeField]
     private Transform[] _flyingCurve;
+    [SerializeField]
+    private float _duration = 1f;
 
     private Vector3[] _flyingCurvePoints;
-    private Vector3 _startPositoin;
 
+    public float Duration { get { return _duration; } }
 
-    // Start is called before the first frame update
     void Start()
     {
-        _startPositoin = transform.position;
-        // transform.localScale = new Vector3(.1f,.1f,.1f);
-
         _flyingCurvePoints = new Vector3[_flyingCurve.Length];
 
-        for (int i = 0; i < _flyingCurve.Length; i++)
+        for (int i = 0; i < _flyingCurve.Length; i++) {
             _flyingCurvePoints[i] = _flyingCurve[i].position;
+        }
 
         var sequence = DOTween.Sequence()
-       .Append(transform.DOPath(_flyingCurvePoints, 5f, PathType.CatmullRom, PathMode.Sidescroller2D, 10, Color.red))
-       .Join(transform.DOScale(new Vector3(5f, 5f, 5f), 1f));
-        sequence.SetLoops(-1, LoopType.Yoyo);
-
-        //transform.DOPath(_flyingCurvePoints, 5f, PathType.CatmullRom, PathMode.Sidescroller2D, 10, Color.red);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+            .Append(transform.DOLocalPath(_flyingCurvePoints, _duration, PathType.CatmullRom, PathMode.Sidescroller2D, 5, Color.red))
+            .Join(transform.DOScale(new Vector3(2f, 2f, 2f), _duration));
     }
 }
--- Assets/Script/UI/LifesUI.cs
+++ Assets/Script/UI/LifesUI.cs
@@ -5,21 +5,42 @@ using TMPro;
 
 public class LifesUI : MonoBehaviour
 {
+    [SerializeField]
+    private LifesMinusEffect _decreaseEffect;
+
     TextMeshProUGUI lifesUI_GUI;
-    // Start is called before the first frame update
+
     void Start()
     {
         lifesUI_GUI = GetComponent<TextMeshProUGUI>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        StartCoroutine(SpawnRepeat());
     }
 
     public void DisplayLifes(int lifes)
     {
         lifesUI_GUI.text = lifes.ToString();
+    }
+
+    private void SpawnDecreaseEffect()
+    {
+        LifesMinusEffect effect = Instantiate(_decreaseEffect, transform.position, Quaternion.identity) as LifesMinusEffect;
+        effect.transform.parent = transform;
+        StartCoroutine(DestroyEffect(effect));
+    }
+
+    private IEnumerator DestroyEffect(LifesMinusEffect effect)
+    {
+        yield return new WaitForSeconds(effect.Duration);
+        Destroy(effect.gameObject);
+    }
+
+    //TODO delet it
+    private IEnumerator SpawnRepeat()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(2f);
+            SpawnDecreaseEffect();
+        }
     }
 }
